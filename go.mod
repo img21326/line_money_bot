96c156780f3a890699ffd731928e7df5784aa004
AndrewLiao <img21326@gmail.com>
2021-05-28 20:41:44 +0800

    移植postgres完成

--- go.mod
+++ go.mod
@@ -6,4 +6,8 @@ require (
 	github.com/influxdata/influxdb-client-go/v2 v2.3.0
 	github.com/jinzhu/now v1.1.2
 	github.com/line/line-bot-sdk-go/v7 v7.9.1
+	github.com/mattn/go-sqlite3 v1.14.7 // indirect
+	gorm.io/driver/postgres v1.1.0 // indirect
+	gorm.io/driver/sqlite v1.1.4 // indirect
+	gorm.io/gorm v1.21.10 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,11 +9,34 @@ import (
 	"strings"
 	"time"
 
-	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
-	"github.com/influxdata/influxdb-client-go/v2/api/write"
 	"github.com/jinzhu/now"
 	"github.com/line/line-bot-sdk-go/v7/linebot"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+type User struct {
+	gorm.Model
+	LineId   string    `json:"line_id"`
+	Accounts []Account `json:"accounts"`
+	Total    int64     `json:"total"`
+	tags     []Tag
+}
+
+type Account struct {
+	gorm.Model
+	Amount int   `json:"amount"`
+	Tags   []Tag `json:"tags"`
+	UserID uint
+}
+
+type Tag struct {
+	gorm.Model
+	Name      string `json:"name"`
+	AccountID uint
+	UserID    uint
+	// Accounts []Account `json:"accounts",gorm:"many2many:account_tags;"`
+}
 
 func main() {
 	bot, err := linebot.New(
@@ -24,11 +46,21 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	client := influxdb2.NewClient(os.Getenv("INFLUXDB_HOST"), os.Getenv("INFLUXDB_TOKEN"))
-	writeAPI := client.WriteAPIBlocking("my-org", "my-bucket")
-	queryAPI := client.QueryAPI("my-org")
+
 	// queryAPI := client.QueryAPI("my-org")
 	// Setup HTTP Server for receiving requests from LINE platform
+	db_host := os.Getenv("POSTGRES_HOST")
+	db_pwd := os.Getenv("POSTGRES_PASSWORD")
+	db_port := os.Getenv("POSTGRES_PORT")
+	dsn := fmt.Sprintf("host=%s user=postgres password=%s dbname=moneybot port=%s sslmode=disable TimeZone=Asia/Taipei", db_host, db_pwd, db_port)
+	// fmt.Print(dsn)
+	// "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db.AutoMigrate(&User{}, &Account{}, &Tag{})
+
+	Repo := NewRepo(db)
+
 	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
 		events, err := bot.ParseRequest(req)
 		if err != nil {
@@ -44,22 +76,13 @@ func main() {
 				switch message := event.Message.(type) {
 				case *linebot.TextMessage:
 					log.Printf("UserID: %v", event.Source.UserID)
+					user := Repo.FindOrCreateUser(event.Source.UserID)
+					log.Printf("Find User: %+v", user)
 					message_arr := strings.Fields(message.Text)
 					log.Printf("Message: %v", message_arr)
 					if len(message_arr) == 1 {
 						if message_arr[0] == "餘額" {
-							_start := "2021-01-01T00:00:01Z"
-							query_str := fmt.Sprintf(`from(bucket:"my-bucket")|> range(start: %s) |> filter(fn: (r) => r._measurement == "account_book") |> filter(fn: (r) => r["unit"] == "%s") |> filter(fn: (r) => r["_field"] == "amount") |> sum()`, _start, event.Source.UserID)
-							// log.Printf("%v", query_str)
-							result, err := queryAPI.Query(context.Background(), query_str)
-							if err != nil {
-								log.Printf("error from 餘額: %v", err)
-							}
-							var amount int64
-							for result.Next() {
-								amount = result.Record().Value().(int64)
-							}
-							if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(fmt.Sprintf("餘額為: %d", amount))).Do(); err != nil {
+							if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(fmt.Sprintf("目前餘額為: %d", user.Total))).Do(); err != nil {
 								log.Print(err)
 							}
 							return
@@ -67,92 +90,74 @@ func main() {
 						if message_arr[0] == "今日花費" {
 							now := time.Now()
 							year, month, day := now.Date()
-							_start := time.Date(year, month, day, 0, 0, 0, 0, now.Location())
-							_end := time.Date(year, month, day, 23, 59, 59, 59, now.Location())
-							query_str := fmt.Sprintf(`from(bucket:"my-bucket")|> range(start: %d, stop: %d) |> filter(fn: (r) => r._measurement == "account_book") |> filter(fn: (r) => r["unit"] == "%s") |> filter(fn: (r) => r["_field"] == "amount") |> filter(fn: (r) => r._value < 0) |> sum()`, _start.Unix(), _end.Unix(), event.Source.UserID)
-							// log.Printf("%v", query_str)
-							result, err := queryAPI.Query(context.Background(), query_str)
-							if err != nil {
-								log.Printf("error from 今日花費: %v", err)
-							}
-							var amount int64
-							for result.Next() {
-								amount = result.Record().Value().(int64)
-							}
-							if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(fmt.Sprintf("本日花費為: %d", Abs((amount))))).Do(); err != nil {
+							start := time.Date(year, month, day, 0, 0, 0, 0, now.Location())
+							end := time.Date(year, month, day, 23, 59, 59, 59, now.Location())
+							total := Repo.GetSumOfAccount(&user, start, end)
+							if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(fmt.Sprintf("今日花費: %d", total))).Do(); err != nil {
 								log.Print(err)
 							}
 							return
-
 						}
 						if message_arr[0] == "本週花費" {
-							_start := now.BeginningOfWeek()
-							_end := now.EndOfWeek()
-							query_str := fmt.Sprintf(`from(bucket:"my-bucket")|> range(start: %d, stop: %d) |> filter(fn: (r) => r._measurement == "account_book") |> filter(fn: (r) => r["unit"] == "%s") |> filter(fn: (r) => r["_field"] == "amount") |> filter(fn: (r) => r._value < 0) |> sum()`, _start.Unix(), _end.Unix(), event.Source.UserID)
-							// log.Printf("%v", query_str)
-							result, err := queryAPI.Query(context.Background(), query_str)
-							if err != nil {
-								log.Printf("error from 本週花費: %v", err)
-							}
-							var amount int64
-							for result.Next() {
-								amount = result.Record().Value().(int64)
-							}
-							if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(fmt.Sprintf("本週花費: %d", Abs((amount))))).Do(); err != nil {
+							start := now.BeginningOfWeek()
+							end := now.EndOfWeek()
+							total := Repo.GetSumOfAccount(&user, start, end)
+							if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(fmt.Sprintf("今日花費: %d", total))).Do(); err != nil {
 								log.Print(err)
 							}
 							return
-
 						}
 						if message_arr[0] == "本月花費" {
-							_start := now.BeginningOfMonth()
-							_end := now.EndOfMonth()
-							query_str := fmt.Sprintf(`from(bucket:"my-bucket")|> range(start: %d, stop: %d) |> filter(fn: (r) => r._measurement == "account_book") |> filter(fn: (r) => r["unit"] == "%s") |> filter(fn: (r) => r["_field"] == "amount") |> filter(fn: (r) => r._value < 0) |> sum()`, _start.Unix(), _end.Unix(), event.Source.UserID)
-							// log.Printf("%v", query_str)
-							result, err := queryAPI.Query(context.Background(), query_str)
-							if err != nil {
-								log.Printf("error from 本月花費: %v", err)
-							}
-							var amount int64
-							for result.Next() {
-								amount = result.Record().Value().(int64)
-							}
-							if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(fmt.Sprintf("本月花費: %d", Abs((amount))))).Do(); err != nil {
+							start := now.BeginningOfMonth()
+							end := now.EndOfMonth()
+							total := Repo.GetSumOfAccount(&user, start, end)
+							if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(fmt.Sprintf("今日花費: %d", total))).Do(); err != nil {
 								log.Print(err)
 							}
 							return
 						}
 					}
+
+					// 如果開頭不是+或-
+					// 則跳出
 					if message_arr[0][0] != 43 && message_arr[0][0] != 45 {
 						if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("格式輸入錯誤")).Do(); err != nil {
 							log.Print(err)
 						}
 						return
 					}
+
+					// 最多輸入兩行
+					// 否則跳出
 					if len(message_arr) >= 3 {
 						if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("格式輸入錯誤")).Do(); err != nil {
 							log.Print(err)
 						}
 						return
 					}
+
+					// Create
+
 					amount, _ := strconv.Atoi(message_arr[0])
+					if amount != 0 {
+						acc := Account{
+							Amount: amount,
+							UserID: user.ID,
+						}
+						if len(message_arr) > 1 {
+							tags := strings.Split(message_arr[1], ",")
+							for _, t := range tags {
+								acc.Tags = append(acc.Tags, Tag{Name: t, UserID: user.ID})
+							}
+						}
 
-					var p *write.Point
-					if len(message_arr) == 2 {
-						p = influxdb2.NewPointWithMeasurement("account_book").
-							AddTag("unit", event.Source.UserID).
-							AddField("amount", amount).
-							AddField("desc", message_arr[1]).
-							SetTime(time.Now())
-					} else {
-						p = influxdb2.NewPointWithMeasurement("account_book").
-							AddTag("unit", event.Source.UserID).
-							AddField("amount", amount).
-							SetTime(time.Now())
+						err := Repo.CreateAccountAndUpdateUser(&user, &acc)
+						if err != nil {
+							log.Print("Create Account Error: %+v", err)
+							return
+						}
 					}
-
-					writeAPI.WritePoint(context.Background(), p)
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("已存入資料庫")).Do(); err != nil {
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(fmt.Sprintf("目前餘額為: %d", user.Total))).Do(); err != nil {
 						log.Print(err)
 					}
 					return
@@ -162,6 +167,7 @@ func main() {
 	})
 	// This is just sample code.
 	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
+	log.Printf("Server Start at Port: %s", os.Getenv("PORT"))
 	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
 		log.Fatal(err)
 	}
@@ -172,4 +178,59 @@ func Abs(x int64) int64 {
 		return -x
 	}
 	return x
+}
+
+type Repo struct {
+	db *gorm.DB
+}
+
+func NewRepo(db *gorm.DB) *Repo {
+	return &Repo{db}
+}
+
+func (r *Repo) FindOrCreateUser(line_id string) (user User) {
+	err := r.db.Where("line_id=?", line_id).Find(&user).Error
+
+	if user.ID == 0 || err == gorm.ErrRecordNotFound {
+		user = User{LineId: line_id, Total: 0}
+		r.db.Create(&user)
+	}
+	return user
+}
+
+// func (r *Repo) CreateAccount(account *Account) error {
+// 	err := r.db.Create(&account).Error
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
+
+func (r *Repo) CreateAccountAndUpdateUser(user *User, account *Account) error {
+	return r.db.Transaction(func(tx *gorm.DB) error {
+		if err := tx.First(&user).Error; err != nil {
+			return err
+		}
+		user.Total += int64(account.Amount)
+		if err := tx.Save(&user).Error; err != nil {
+			return err
+		}
+
+		if err := tx.Create(&account).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+func (r *Repo) GetSumOfAccount(user *User, start time.Time, end time.Time) int64 {
+	type Result struct {
+		Total int64
+	}
+	var result Result
+	err := r.db.Model(&Account{}).Select("user_id, sum(amount) as Total").Where("user_id=?", user.ID).Where("amount < 0").Where("created_at>?", start).Where("created_at<=?", end).Group("user_id").Find(&result).Error
+	if err != nil {
+		log.Fatalf("Get Sum Of Account Error: %+v", err)
+	}
+	return result.Total
 }
